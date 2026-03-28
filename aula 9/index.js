05b570b80bf206b47836606ae103c26db311506f
Leonardo Thome <158233688+LeoDev991@users.noreply.github.com>
2026-03-28 19:50:59 -0300

    .

--- aula 9/index.js
+++ aula 9/index.js
@@ -1,9 +1,12 @@
 function formataData(data) {
     const dia = data.getDate();
-    const mes = data.getMonth();
+    const mes = data.getMonth() + 1;
     const ano = data.getFullYear();
     const hora = data.getHours();
     const minuto = data.getMinutes();
+    const segundos = data.getSeconds();
+
+    return `${dia}/${mes}/${ano} ${hora}:${minuto}:${segundos}`
 }
 
 const data = new Date();
