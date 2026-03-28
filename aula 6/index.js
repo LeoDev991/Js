d8a99cfd837bdcc781974430393beff85ab1a1a1
Leonardo Thome <158233688+LeoDev991@users.noreply.github.com>
2026-03-28 11:32:03 -0300

    Create index.js

---
+++ aula 6/index.js
@@ -0,0 +1,10 @@
+const hora = 36;
+if (hora >= 0 && hora <= 12) {
+    console.log(`Bom dia!`)
+} else if (hora >= 13 && hora <= 18) {
+    console.log(`Boa Tarde!`)
+} else if (hora >= 19 && hora <= 23)
+console.log(`Boa Noite!!`)
+else {
+    console.log(`Olá`)
+}
