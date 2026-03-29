336b9ace82606d6116795cd00c7f1cf6176131c1
Leonardo Thome <158233688+LeoDev991@users.noreply.github.com>
2026-03-29 20:13:11 -0300

    Aprendendo um pouco sobre For em JS

---
+++ aula 11/index.js
@@ -0,0 +1,17 @@
+// Aula sobre For
+
+
+// For In --> Ele lê os indices ou chaves do objeto
+const frutas = [`Pera`, `Maçã`, `Jabuticaba`];
+for (let index in frutas) {
+    console.log(frutas[index]);
+}
+
+
+//Fo Of --> Ele lê os valores
+
+
+//for (let i = 0;  i <= 5000; i+= 4) {
+ //   console.log(`Linha ${i}`);
+//}
+
