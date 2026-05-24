b0e6d4adb94003c5b7abb1ec2a0e508bae4e6b06
Leonardo Thome <158233688+LeoDev991@users.noreply.github.com>
2026-05-24 16:17:16 -0300

    .

---
+++ aula 26/index.js
@@ -0,0 +1,8 @@
+//const nome = "Leonardo Thome da Costa";
+//const nomes = nome.split(" ");
+
+const nomes = ["Leonardo", "Thome", "da", "Costa"];
+
+const nomeCompleto = nomes.join(" ");
+
+console.log(nomeCompleto);
