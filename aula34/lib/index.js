859d785d80905ca273cfce54dedf72cf0d98fc0d
Leonardo Thome <158233688+LeoDev991@users.noreply.github.com>
2026-07-16 15:24:11 -0300

    .

--- aula34/lib/index.js
+++ aula34/lib/index.js
@@ -1,6 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const produto = { nome: 'Caneca', preco: 19.90 };
-const outraCoisa = produto;
-produto.nome = 'Leonardo Thome';
+const outraCoisa = {
+    ...produto,
+    material: 'Porcelana'
+};
+
+outraCoisa.nome = 'Leonardo Thome';
+outraCoisa.preco = 29.90;
 console.log(outraCoisa);
--- aula34/src/index.ts
+++ aula34/src/index.ts
@@ -1,5 +1,4 @@
+
 const produto = {nome: 'Caneca' , preco: 19.90}
 const outraCoisa = produto;
 
-produto.nome = 'Leonardo Thome';
-console.log(outraCoisa);
