c986f2103bda0ac6d7832ff80702cc85e34a3c1a
Leonardo Thome <158233688+LeoDev991@users.noreply.github.com>
2026-07-18 20:15:32 -0300

    Add aula35: constructor function example
    Demonstrates JavaScript constructor functions with the pessoa class, showing object instantiation with the new keyword and instance property creation.

---
+++ aula35/index.js
@@ -0,0 +1,14 @@
+// Construtora
+
+function pessoa (nome, sobrenome) {
+    this.nome = nome
+    this.sobrenome = sobrenome
+    this.nomeCompleto = () => this.nome + ' ' + this.sobrenome;
+}
+
+//. Instancia
+const pessoa1 = new pessoa('Leonardo', 'Thome')
+const pessoa2 = new pessoa('Maria', 'Silva')
+
+console.dir(pessoa1)
+console.dir(pessoa2)
