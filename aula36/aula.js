0276392a3289c924fd0fb2f49043fb94241d2077
Leonardo Thome <158233688+LeoDev991@users.noreply.github.com>
2026-07-22 13:33:28 -0300

    Update aula.js

--- aula36/aula.js
+++ aula36/aula.js
@@ -1,4 +1,4 @@
-function Produto (nome, preco) {
+function Produto(nome, preco) {
     this.nome = nome;
     this.preco = preco;
 }
@@ -11,10 +11,28 @@ Produto.prototype.aumento = function (percentual) {
     this.preco = this.preco + (this.preco * (percentual / 100));
 }
 
-const p1 = new Produto ('Camiseta', 50);
+const p1 = new Produto('Camiseta', 50);
 p1.desconto(10);
 console.log(p1);
 
-const p2 = new Produto ('Caneca', 15);
+const p2 = new Produto('Caneca', 15);
 p2.aumento(20);
 console.log(p2);
+
+const p3 = Object.create(Produto.prototype, {
+    tamanho: {
+        value: 'M',
+        enumerable: true,
+        writable: true,
+        configurable: true
+    },
+
+    tamanho2: {
+        value: 'GG',
+        enumerable: true,
+        writable: true,
+        configurable: true
+    }
+});
+p3.aumento(30);
+console.log(p3);
