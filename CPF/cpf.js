05abfd4205fe73acb4a11613a0a850192d56fe1c
Leonardo Thome <158233688+LeoDev991@users.noreply.github.com>
2026-08-03 13:28:28 -0300

    Finalizado
    Verificador de CPF criado com suecesso!!

--- CPF/cpf.js
+++ CPF/cpf.js
@@ -9,27 +9,40 @@ function ValidaCPF(cpfEnviado) {
 ValidaCPF.prototype.valida = function () {
     if (typeof this.cpfLimpo === 'undefined') return false;
     if (this.cpfLimpo.length !== 11) return false;
+    if (this.isSequencia()) return false;
 
     const cpfParcial = this.cpfLimpo.slice(0, -2); 
     const digito1 = this.digitoValido(cpfParcial);
-    //const digito2 = this.digitoValido(cpfParcial + digito1)
-    //return this.cpfLimpo === cpfParcial + digito1 + digito2;
-    return true;
+    const digito2 = this.digitoValido(cpfParcial + digito1)
+    
+    const novoCpf = cpfParcial + digito1 + digito2;
+    return novoCpf === this.cpfLimpo;
 
 }
 
 ValidaCPF.prototype.digitoValido = function (cpfParcial) {
     const cpfArray = Array.from(cpfParcial);
     let regressivo = cpfArray.length + 1;
-    const digito = cpfArray.reduce((ac, val) => {
+    const total = cpfArray.reduce((ac, val) => {
         ac += (regressivo * Number(val)); 
         regressivo --;
         console.log(regressivo, val, regressivo * val)
         return ac;
     }, 0)
 
-    console.log(digito);
+    const digito = 11 - (total % 11);
+    return digito > 9 ? '0' : String(digito);
+};
+
+ValidaCPF.prototype.isSequencia = function () {
+    const sequencia = this.cpfLimpo[0].repeat(this.cpfLimpo.length);
+    return sequencia === this.cpfLimpo;
 }
 
 const cpf = new ValidaCPF('070.987.720-03');
-console.log(cpf.valida());
+
+if (cpf.valida()) {
+    console.log('CPF válido');
+} else {
+    console.log('CPF inválido');
+}
